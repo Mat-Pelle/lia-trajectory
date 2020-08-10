50b3b191a8bc4b2b561240050d506f078ae4d8bc
Mat-Pelle <69264404+Mat-Pelle@users.noreply.github.com>
2020-08-10 18:00:00 -0300

    Create Vehicle.py
    The Vehicle class, where all variables related to the vehicle are calculated and initialized

---
+++ Vehicle.py
@@ -0,0 +1,21 @@
+Class Vehicle:
+    #First comes the initialization of all vehicle variables
+    
+    #Mass characteristics
+    mass_empty =
+    mass_full =
+    
+    #Aerodynamic characteristics
+    
+    
+    def __init__(#insert all parameters here):
+        #Here all variable initialization and initial calculations are carried out
+    
+    
+    #Here other functions (defined afterwards) can be called for necessesary calculations. For example:
+    def moment_inertia_calc(#insert all relevant parameters here):
+        
+    #or
+    def cd_vs_mach():
+        
+        
